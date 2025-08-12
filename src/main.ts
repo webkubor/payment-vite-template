67db87928567a3c93bf64cef9a112182b6611e8e
webkubor <webkubor@163.com>
2025-08-12 18:21:04 +0800

    feat 提交

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
-import { useMessagePlugin } from "@/plugins/message";
-import 'dayjs/locale/zh-cn'
-
-// 导入全局样式
+import { useMessagePlugin } from "./plugins/message";
 import '@/styles/global.scss'
 
 // 创建应用实例
